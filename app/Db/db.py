f2ee0adaf472e64313ceb110aa7af9a4021000bb
AxelGod <43476984+AxelGod@users.noreply.github.com>
2023-11-22 18:47:15 -0400

    Peaches

---
+++ app/Db/db.py
@@ -0,0 +1,18 @@
+import psycopg2
+import os
+class db:
+    def __init__(self):
+        self.conexión= psycopg2.connect(
+                                        host="dpg-clf5qlrl00ks73a6mdp0-a.oregon-postgres.render.com",
+                                        database="dborigin",
+                                        user="dborigin_user",
+                                        password="64D6o2sE41x3onuFu7CEQb8SeAJRCtiR"
+                                       )
+    def ejecutarSentencia(self, sentencia, valores=None):
+        cursor = self.conexión.cursor()
+        if valores:
+            cursor.execute(sentencia, valores)
+        else:
+            cursor.execute(sentencia)
+        self.conexión.commit()
+        return cursor
---
+++ app/Db/dbadapter.py
@@ -0,0 +1,19 @@
+#db adapter
+from app.Db.db import db
+
+class dbadapter:
+    def __init__(self):
+        self.db = db()
+    
+    def crearUsuario(self,codigo, nombre, nacionalidad, identificacion, fecha_ingreso, fecha_salida, modo_ingreso, empresa):
+        try:
+            sentencia_sql = 'INSERT INTO public."Origin" ("Código", "Nombre_y_Apellido", "Nacionalidad", "identificación", "Fecha_de_ingreso", "Fecha_de_salida", "Modo_de_ingreso", "Empresa") VALUES (%s, %s, %s, %s, %s, %s, %s, %s);'
+
+            valores = (codigo, nombre, nacionalidad, identificacion, fecha_ingreso, fecha_salida, modo_ingreso, empresa)
+
+            self.db.ejecutarSentencia(sentencia_sql, valores)
+
+
+            print("Se inserto el usuario con info: " + str(codigo) + " " + str(nombre) + " " + str(nacionalidad) + " " + str(identificacion) + " " + str(fecha_ingreso) + " " + str(fecha_salida) + " " + str(modo_ingreso) + " " + str(empresa))
+        except Exception as e:
+            print(e)
